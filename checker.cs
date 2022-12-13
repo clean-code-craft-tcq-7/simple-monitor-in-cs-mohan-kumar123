b9302457da4b8e370258128a789102f5f14ea8e7
QOH2KOR <QOH2KOR@BMHE1115826>
2022-12-13 19:31:25 +0530

    First commit on master

--- checker.cs
+++ checker.cs
@@ -32,6 +32,7 @@ class Checker
     static int Main() {
         ExpectTrue(batteryIsOk(25, 70, 0.7f));
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
+        ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
         return 0;
     }
